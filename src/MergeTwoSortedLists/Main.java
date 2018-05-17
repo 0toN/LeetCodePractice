f2deb8c1aa2964bf6aa682df69f813c121f7afd0
Adam <Adam@baida.im>
2018-05-17 14:25:30 +0800

    #21: Merge Two Lists

---
+++ src/MergeTwoSortedLists/Main.java
@@ -0,0 +1,42 @@
+package MergeTwoSortedLists;
+
+/**
+ * #21
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 示例：
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class Main {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.data < l2.data) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+
+    public class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int x) {
+            data = x;
+        }
+    }
+}
